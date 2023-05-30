7fd5a8b8aa20106644ae185afb83ee049b3f3ced
Laclac <ngochths150417@fpt.eedu.vn>
2023-05-30 08:07:18 +0700

    fix signup_a.php

--- signup_a.php
+++ signup_a.php
@@ -1,34 +1,55 @@
 <?php
-if(!preg_match("/^\s*[a-zA-Z0-9_]{5,30}\s*$/",$_POST['pass']))
-{ 
+if (!preg_match("/^\s*[a-zA-Z0-9_]{5,30}\s*$/", $_POST['pass'])) {
     header('Location: signup.php?password=error');
-}
-else if ($_POST['pass'] != $_POST['cpass']){   
+    exit;
+} elseif ($_POST['pass'] != $_POST['cpass']) {
     header('Location: signup.php?confirm=error');
-}
-else if (!preg_match("/^\s*[a-z0-9_]{4,30}\s*$/",$_POST['username'])){
+    exit;
+} elseif (!preg_match("/^\s*[a-z0-9_]{5,30}\s*$/", $_POST['username'])) {
     header('Location: signup.php?username=error');
-}
-else{
-    $con12 = new mysqli("localhost","kali","kali","class");
-    if ($con12->connect_error) {
-        die("Connection failed: " . $con12->connect_error);
-    } 
-    $stmt = $con12->prepare("SELECT * FROM teacher WHERE username=?");
-    $stmt->bind_param("s", $_POST['username']);
-    $stmt->execute();
-    if($stmt->fetch() == 1){
-        header('Location: signup.php?status=failed');
+    exit;
+} else {
+    $serverName = "DESKTOP-NCJ03T3\MSSQLSERVER02";
+    $database = "php_hardcore_project";
+    $uid = "";
+    $pass = "";
+
+    $connectionOptions = [
+        "Database" => $database,
+        "Uid" => $uid,
+        "PWD" => $pass
+    ];
+
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    if ($conn === false) {
+        die(print_r(sqlsrv_errors(), true));
     }
-    else{
-        $con13 = new mysqli("localhost","kali","kali","class");
-        $stmt1 = $con13->prepare('INSERT INTO teacher VALUES (?,?)');
-        $stmt1->bind_param("ss", $_POST['username'],$_POST['pass']);
-        if($stmt1->execute()){    // 
-            header('Location: signup.php?status=success');
-        } else {
+
+    $tsql = "SELECT * FROM teacher WHERE username = ?";
+    $params = array($_POST['username']);
+    $stmt = sqlsrv_query($conn, $tsql, $params);
+    if ($stmt === false) {
+        die(print_r(sqlsrv_errors(), true));
+    }
+
+    if (sqlsrv_fetch($stmt) === true) {
+        header('Location: signup.php?status=failed');
+        exit;
+    } else {
+        sqlsrv_free_stmt($stmt);
+
+        $tsql = "INSERT INTO teacher (username, password) VALUES (?, ?)";
+        $params = array($_POST['username'], $_POST['pass']);
+        $stmt = sqlsrv_query($conn, $tsql, $params);
+        if ($stmt === false) {
             header('Location: signup.php?status=failed');
+            exit;
+        } else {
+            header('Location: signup.php?status=success');
+            exit;
         }
     }
-} 
+
+    sqlsrv_close($conn);
+}
 ?>
