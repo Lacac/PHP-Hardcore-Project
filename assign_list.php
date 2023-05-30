c87c1644f067fbf2a67952dc660665750210e844
Laclac <ngochths150417@fpt.eedu.vn>
2023-05-30 07:59:19 +0700

    fix assign_list.php

--- assign_list.php
+++ assign_list.php
@@ -1,16 +1,18 @@
 <?php
 session_start();
-if(!isset($_SESSION['teacher']) || !isset($_SESSION['student'])){
+if (!isset($_SESSION['teacher']) && !isset($_SESSION['student'])) {
     // Redirect them to the login page
-    header("Location: login.php");  
-} 
+    header("Location: login.php");
+    exit(); // Terminate script execution after redirection
+}
+
 $fileList = glob('baitap/*');
-foreach($fileList as $filename){
-    if(is_file($filename)){
-	$target = basename("$filename",".pdf").PHP_EOL;
-	$mypath="/var/www/website/baitap/$target";
-	mkdir($mypath, 0777, true);
+foreach ($fileList as $filename) {
+    if (is_file($filename)) {
+        $target = basename("$filename", ".pdf") . PHP_EOL;
+        $mypath = "/var/www/website/baitap/$target";
+        mkdir($mypath, 0777, true);
         echo $target . '<a href="' . $filename . '"> Download </a>' . '<a href=submit.php> Nop bai </a>' . '<a href=view_submitted.php> Bai da nop </a>', '<br>';
-    }   
+    }
 }
 ?>
