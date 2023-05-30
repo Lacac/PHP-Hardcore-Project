7813da276558c9fff81708479f1128c8695e6b20
Laclac <ngochths150417@fpt.eedu.vn>
2023-05-30 08:12:18 +0700

    fix uploadchallenge.php

--- uploadchallenge.php
+++ uploadchallenge.php
@@ -1,30 +1,49 @@
 <?php
 include '../challenge.php';
+
+$serverName = "localhost";
+$connectionOptions = array(
+    "Database" => "game",
+    "Uid" => "kali",
+    "PWD" => "kali"
+);
+
 // This is the directory where images will be saved
 $target_dir = "challenge/";
-$target_file = $target_dir . basename( $_FILES["filesToUpload"]["name"]);
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 
 // This gets all the other information from the form
-$hints=$_POST["hints"];
-$challenge=($_FILES["filesToUpload"]["name"]);
+$hints = $_POST["hints"];
+$challenge = $_FILES["fileToUpload"]["name"];
 
-// Connects to your Database
+// Connect to MSSQL
+$conn = sqlsrv_connect($serverName, $connectionOptions);
 
-$con11 = new mysqli("localhost","kali","kali","game");
+if ($conn === false) {
+    echo "Unable to connect to MSSQL: " . print_r(sqlsrv_errors(), true);
+    exit;
+}
+
 // Writes the information to the database
-$sql = "INSERT INTO challenge (challenge,hints) VALUES ('$challenge', '$hints')";
-mysqli_query($con11,$sql);
+$sql = "INSERT INTO challenge (challenge, hints) VALUES (?, ?)";
+$params = array($challenge, $hints);
+$stmt = sqlsrv_query($conn, $sql, $params);
+
+if ($stmt === false) {
+    echo "Error inserting data into the database: " . print_r(sqlsrv_errors(), true);
+    exit;
+}
 
 // Writes the photo to the server
-if(move_uploaded_file($_FILES["filesToUpload"]["tmp_name"], $target_file))
-{
-
-// Tells you if its all ok
-echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded, and your information has been added to the directory";
+if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    // Tells you if it's all okay
+    echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded, and your information has been added to the directory";
+} else {
+    // Gives an error if it's not
+    echo "Sorry, there was a problem uploading your file.";
 }
-else {
 
-// Gives and error if its not
-echo "Sorry, there was a problem uploading your file.";
-}
+// Clean up resources
+sqlsrv_free_stmt($stmt);
+sqlsrv_close($conn);
 ?>
