01ef155876e7afed46da617ef4399d48bf8de716
Laclac <ngochths150417@fpt.eedu.vn>
2023-05-30 08:10:39 +0700

    fix up_assign.php

--- up_assign.php
+++ up_assign.php
@@ -2,30 +2,46 @@
 $target_dir = "baitap/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$pdfFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$pdfFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+$errors = [];
+
+// Check if file already exists
 if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
+  $errors[] = "Sorry, the file already exists.";
   $uploadOk = 0;
 }
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 99999999999999999999) {
-  echo "Sorry, your file is too large.";
+
+// Check file size (limit it to a reasonable size)
+$maxFileSize = 10 * 1024 * 1024; // 10MB
+if ($_FILES["fileToUpload"]["size"] > $maxFileSize) {
+  $errors[] = "Sorry, the file is too large. Please upload a file up to 10MB in size.";
   $uploadOk = 0;
 }
-// Allow certain file formats
-if($pdfFileType != "pdf") {
-  echo "Xin loi ban chi duoc phep upload file pdf.";
+
+// Allow only PDF files
+if ($pdfFileType != "pdf") {
+  $errors[] = "Sorry, only PDF files are allowed.";
   $uploadOk = 0;
 }
+
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-  echo "Xin loi, file chua duoc upload thanh cong";
-// if everything is ok, try to upload file
+  $errors[] = "Sorry, the file was not uploaded.";
 } else {
+  // Try to upload the file
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "File ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " da duoc upload thanh cong.";
+    $message = "File " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded successfully.";
   } else {
-    echo "Xin loi, da co van de khi upload file cua ban.";
+    $errors[] = "Sorry, there was an error uploading your file.";
   }
+}
+
+// Display errors and success message (if any)
+if (!empty($errors)) {
+  foreach ($errors as $error) {
+    echo $error . "<br>";
+  }
+} else {
+  echo $message;
 }
 ?>
