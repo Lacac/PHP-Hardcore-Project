53f1b1a5537a0f77d7862c72b136a7e1869d97cb
DucDMleebee <83963452+DucDMleebee@users.noreply.github.com>
2023-06-05 22:53:32 +0700

    Update assign_list.php

--- assign_list.php
+++ assign_list.php
@@ -9,11 +9,10 @@ if (!isset($_SESSION['teacher']) && !isset($_SESSION['student'])) {
 $fileList = glob('baitap/*');
 foreach ($fileList as $filename) {
     if (is_dir($filename)) {
-        // $target = basename("$filename", ".pdf") . PHP_EOL;
-        // $mypath = "./baitap/$target";
-        $mypath = "./$filename";
-        mkdir($mypath, 0777, true);
-        echo $mypath . '<a href="' . $filename . '"> Download </a>' . '<a href=submit.php> Nop bai </a>' . '<a href=view_submitted.php> Bai da nop </a>', '<br>';
+        $assignment = basename($filename);
+        $submitLink = "submit.php?assignment=" . urlencode($assignment);
+        $viewSubmittedLink = "view_submitted.php?assignment=" . urlencode($assignment);
+        echo $filename . '<a href="' . $filename . '"> Download </a>' . '<a href="' . $submitLink . '"> Nop bai </a>' . '<a href="' . $viewSubmittedLink . '"> Bai da nop </a>', '<br>';
     }
 }
 ?>
